25199b2c0ee2b24b6b13d87394a95923dbed2a84
florpi <forpisoteadadejardin@gmail.com>
2019-09-26 22:58:45 +0100

    Initial commit, working with comet ml

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze the galaxy halo connection found in hydordynamical simulations using Machine Learning techniques.',
+    author='Carolina Cuesta and Christoph Becker',
+    license='MIT',
+)
