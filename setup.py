2bf07c1187a6cf9318997c38156469235c3eb989
florpi <forpisoteadadejardin@gmail.com>
2019-10-26 13:16:24 +0100

    tree structure modified from src to package name, haas env updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="gahaco",
     packages=find_packages(),
     version="0.1.0",
     description="Analyze the galaxy halo connection found in hydordynamical simulations using Machine Learning techniques.",
