120dd8b8283975a65ae755b3d87b81725ad2daa0
florpi <forpisoteadadejardin@gmail.com>
2019-11-11 22:16:37 +0000

    full training script, added correlation hierarchy cuts

---
+++ gahaco/features/correlation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from scipy.stats import spearmanr
+from scipy.cluster import hierarchy
+from collections import defaultdict
+
+def select_uncorrelated_features(features_df):
+	'''
+	Clusters the Spearman rank-roder correlation of the different features, we keep only a single feature per cluster
+
+	Args:
+		features_df: pandas data frame of features
+	Returns:
+		reduced dataframe with only low correlated features
+	'''
+	corr = spearmanr(features_df).correlation
+	corr_linkage = hierarchy.ward(corr)
+	cluster_ids = hierarchy.fcluster(corr_linkage, 1, criterion='distance')
+	cluster_id_to_feature_ids = defaultdict(list)
+	for idx, cluster_id in enumerate(cluster_ids):
+		cluster_id_to_feature_ids[cluster_id].append(idx)
+	selected_features = [v[0] for v in cluster_id_to_feature_ids.values()]
+
+	return features_df[features_df.columns[selected_features].values]
