69888595577f0dfb2c5328e17fac9c2f028aa76b
Christoph Becker <christoph.becker@durham.ac.uk>
2020-01-16 16:29:04 +0000

    working version

--- gahaco/features/build_features.py
+++ gahaco/features/build_features.py
@@ -2,15 +2,16 @@ import pandas as pd
 import h5py
 import numpy as np
 
-boxsize = 100
+boxsize = 300
 halo_mass_cut = 1.e11
 
+output_file = "merged_dataframe_v2.h5"
 data_path = '/cosma7/data/dp004/dc-cues1/tng_dataframes/'
 mergertree_file = data_path + 'TNG%dDark_Hydro_MergerTree.hdf5' % (boxsize)
 subfind_dark_file = data_path + 'TNG%ddark_subfind.hdf5' % (boxsize)
 subfind_hydro_file = data_path + 'TNG%dhydro_subfind.hdf5' % (boxsize)
-particle_outer_file = data_path + 'TNG%ddark_particle_ss_r200c.hdf5' % (boxsize)
-particle_inner_file = data_path + 'TNG%ddark_particle_ss_r2500c.hdf5' % (boxsize)
+particle_outer_file = data_path + 'TNG%ddark_halo_particle_summary_r200c.hdf5' % (boxsize)
+particle_inner_file = data_path + 'TNG%ddark_halo_particle_summary_r2500c.hdf5' % (boxsize)
 
 # Read MergerTree Data ----------------------------------------------------------
 mt_df = pd.read_hdf(mergertree_file)
@@ -18,59 +19,66 @@ dmo_df = mt_df.loc[mt_df['M200_HYDRO'] > halo_mass_cut]
 
 # Read SubFind Data -------------------------------------------------------------
 sf_df = pd.read_hdf(subfind_dark_file)
-dmo_df = pd.merge(sf_df, dmo_df, on = ['ID_DMO'], how = 'inner')
-
-# Check the merged haloes are the correct ones ----------------------------------
+sf_df = sf_df.loc[10**sf_df['Group_M_Crit200'] > halo_mass_cut]
+dmo_df = pd.merge(sf_df, dmo_df, on=['ID_DMO'], how='inner')
 np.testing.assert_allclose(
-    dmo_merged_df.M200_DMO, dmo_merged_df.Group_M_Crit200, rtol=1e-3
+    10**dmo_df.Group_M_Crit200, dmo_df.M200_DMO, rtol=1e-3
 )
-dmo_merged_df = dmo_merged_df.drop(columns = ['Group_M_Crit200','Group_R_Crit200'])
+dmo_df = dmo_df.drop(columns = ['Group_M_Crit200'])
 
 # Read Particle Data ------------------------------------------------------------
+hubble = 0.6774
 # R200c
 pouter_df = pd.read_hdf(particle_outer_file)
+pouter_df["m200c"] = pouter_df["m200c"].apply(lambda x: x*hubble)
+pouter_df = pouter_df.loc[pouter_df['m200c'] > halo_mass_cut]
+dmo_df = pd.merge(pouter_df, dmo_df, on=['ID_DMO'], how='inner')
 np.testing.assert_allclose(
-    pouter_df.m200c, pouter_df.Group_M_Crit200, rtol=1e-3
+    dmo_df.m200c, dmo_df.M200_DMO, rtol=1e-3
 )
-pouter_df = particle_df.drop(columns = ['m200c'])
-dmo_df = pd.merge(dmo_df, pouter_df, on=['ID_DMO'])
+dmo_df = dmo_df.drop(columns = ['m200c'])
 
 # R2500c
-pinner_df = pd.read_hdf(particle_inner_file)
+pinner_df = pd.read_hdf(particle_outer_file)
+pinner_df["m200c"] = pinner_df["m200c"].apply(lambda x: x*hubble)
+pinner_df = pinner_df.loc[pinner_df['m200c'] > halo_mass_cut]
+dmo_df = pd.merge(pinner_df, dmo_df, on=['ID_DMO'], how='inner')
 np.testing.assert_allclose(
-    inner_df.m200c, dmo_merged_df.Group_M_Crit200, rtol=1e-3
+    dmo_df.m200c, dmo_df.M200_DMO, rtol=1e-3
 )
-pinner_df = pinner_df.drop(columns = ['m200c'])
-
-dmo_df = pd.merge(dmo_df, pinner_df, on=['ID_DMO'])
+dmo_df = dmo_df.drop(columns = ['m200c'])
 
 # Read in properties from tng hydro ---------------------------------------------
 hydro_df = pd.read_hdf(subfind_hydro_file)
 
 hydro_merged_df = pd.merge(
-    dmo_merged_df,
+    dmo_df,
     hydro_df,
     on=['ID_HYDRO'],
     how='inner',
     suffixes=('_dmo', '_hydro')
 )
-
 np.testing.assert_allclose(
-    hydro_merged_df.M200_HYDRO, hydro_merged_df.Group_M_Crit200, rtol=1e-3
+    hydro_merged_df.M200_HYDRO, 10**hydro_merged_df.Group_M_Crit200, rtol=1e-3
 )
-hydro_merged_df = hydro_merged_df.drop(columns = ['Group_M_Crit200', 'M200_DMO'])
-hydro_merged_df.M200c = np.log10(hydro_merged_df.M200c)
+hydro_merged_df = hydro_merged_df.drop(columns=['Group_M_Crit200', 'M200_DMO'])
+print(hydro_merged_df.columns)
 
-# Since matching is not 1-to-1, sum all the galaxies
+# Since matching is not 1-to-1, sum all the galaxies ----------------------------
+# TODO: reconsider the association criteria
 n_gals_total = hydro_merged_df.groupby('ID_DMO')['N_gals'].sum()
-m_stars_total = hydro_merged_df.groupby('ID_DMO')['M_stars'].sum()
-# Drop duplicates
-no_duplicates_df = hydro_merged_df.drop_duplicates(subset = 'ID_DMO', keep = 'last')
-# Add column with total galaxies
-no_duplicates_df = pd.merge(no_duplicates_df, n_gals_total.to_frame('N_gals_total').reset_index())
-no_duplicates_df = pd.merge(no_duplicates_df, m_stars_total.to_frame('M_stars_total').reset_index())
-no_duplicates_df = no_duplicates_df.drop(columns = ['N_gals', 'M_stars'])
-no_duplicates_df.rename(columns = {'N_gals_total':'N_gals', 'M_stars_total':'M_stars'}, inplace = True)
+m_stars_total = hydro_merged_df.groupby('ID_DMO')['M_stars_central'].sum()
+
+# Drop duplicates ---------------------------------------------------------------
+no_duplicates_df = hydro_merged_df.drop_duplicates(subset='ID_DMO', keep='last')
+
+# Add column with total galaxies ------------------------------------------------
+no_duplicates_df = pd.merge(
+    no_duplicates_df, n_gals_total.to_frame('N_gals').reset_index()
+)
+no_duplicates_df = pd.merge(
+    no_duplicates_df, m_stars_total.to_frame('M_stars_central').reset_index()
+)
 
 # Save final dataframe!
 
