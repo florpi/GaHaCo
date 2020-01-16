be56193f65cb20a33c97e953f2a5bef38c215b06
Christoph Becker <christoph.becker@durham.ac.uk>
2020-01-16 09:33:36 +0000

    updated particle feature; and merging of dataset

--- gahaco/features/build_features.py
+++ gahaco/features/build_features.py
@@ -2,109 +2,62 @@ import pandas as pd
 import h5py
 import numpy as np
 
-
-additional_data_path = '/cosma5/data/dp004/hvrn44/HOD/'
-output_file = 'merged_dataframe.h5'
-data_path = '/cosma7/data/dp004/dc-cues1/tng_dataframes/'
-anisotropy_path = "/cosma7/data/dp004/dc-beck3/vel_ani_param.hdf5"
-dmo_file =  'dmo_halos.hdf5'
-hydro_file = 'hydro_galaxies.hdf5'
-matching_file = 'MatchedHaloes_L205n2500.dat'
-additional_properties_file = 'HaloProfiles_DMO_z0.00_ext.hdf5'
-mergertree_file = 'MergerTree_L205n2500TNG_DM_ext_New.hdf5'
-halo_particles_file = 'halo_particle_summary.hdf5'
-inner_file = 'TNG300dark_halo_particle_ss_r2500c.hdf5'
+boxsize = 100
 halo_mass_cut = 1.e11
 
-# ------------------ Halo matching between dmo and hydro simulations
+data_path = '/cosma7/data/dp004/dc-cues1/tng_dataframes/'
+mergertree_file = data_path + 'TNG%dDark_Hydro_MergerTree.hdf5' % (boxsize)
+subfind_dark_file = data_path + 'TNG%ddark_subfind.hdf5' % (boxsize)
+subfind_hydro_file = data_path + 'TNG%dhydro_subfind.hdf5' % (boxsize)
+particle_outer_file = data_path + 'TNG%ddark_particle_ss_r200c.hdf5' % (boxsize)
+particle_inner_file = data_path + 'TNG%ddark_particle_ss_r2500c.hdf5' % (boxsize)
 
-matching_df = pd.read_csv(additional_data_path + matching_file,
-                         delimiter = ' ', skiprows = 1,
-                names = ['ID_DMO', 'ID_HYDRO', 'M200_DMO', 'M200_HYDRO'])
+# Read MergerTree Data ----------------------------------------------------------
+mt_df = pd.read_hdf(mergertree_file)
+dmo_df = mt_df.loc[mt_df['M200_HYDRO'] > halo_mass_cut]
 
-# Apply mass cut
+# Read SubFind Data -------------------------------------------------------------
+sf_df = pd.read_hdf(subfind_dark_file)
+dmo_df = pd.merge(sf_df, dmo_df, on = ['ID_DMO'], how = 'inner')
 
-mass_matching_df = matching_df.loc[matching_df['M200_HYDRO'] > halo_mass_cut]
-
-# ----------- Read in additionally halo properties from the dmo simulation
-
-with h5py.File(additional_data_path + additional_properties_file,  'r') as hf:
-        
-    mass = hf['Haloes']['M200'][:]
-    cnfw = hf['Haloes']['Cnfw'][:]
-    rmax = hf['Haloes']['Rmax'][:]
-    r200c = hf['Haloes']['R200'][:]
-    rhosnfw = hf['Haloes']['Rhosnfw'][:]
-    properties_ids = hf['Haloes']['GroupNumber'][:]
-
-properties = np.vstack([properties_ids,mass, rmax,
-                                     np.log10(r200c), cnfw, rhosnfw]).T
-
-
-properties_df = pd.DataFrame(data = properties,
-                             columns = ['ID_DMO', 'M200c', 'Rmax',
-                                        'R200c', 'Cnfw', 'Rhosnfw'])
-
-merged_matching_df = pd.merge(mass_matching_df, properties_df, on = ['ID_DMO'], how = 'inner')
-
- 
-# ----------- Read in properties from the merger trees 
-with h5py.File(additional_data_path + mergertree_file, 'r') as hf:
-
-    formation_time = hf['Haloes']['z0p50'][:]
-    n_mergers = hf['Haloes']['NMerg'][:]
-    mass_peak = hf['Haloes']['Mpeak'][:]
-    vpeak = hf['Haloes']['Vpeak'][:]
-    mergertree_ids = hf['Haloes']['Index'][:]
-
-mergertree_data = np.vstack([mergertree_ids, formation_time, n_mergers,
-                            np.log10(mass_peak), vpeak]).T
-
-mergertree_df = pd.DataFrame(data = mergertree_data, 
-                columns = ['ID_DMO', 'Formation Time', 'Nmergers','MassPeak', 'vpeak'])
-
-merged_tree_df = pd.merge(merged_matching_df, mergertree_df, on = ['ID_DMO'], how = 'inner')
-
-# ----------- Read in properties from tng dmo
-
-dmo_df = pd.read_hdf(data_path + dmo_file)
-
-dmo_merged_df = pd.merge(merged_tree_df, dmo_df, on = ['ID_DMO'], how = 'inner')
-
-# Check the merged haloes are the correct ones
-np.testing.assert_allclose(dmo_merged_df.M200_DMO, 10**dmo_merged_df.Group_M_Crit200, rtol = 1e-3)
-dmo_merged_df = dmo_merged_df.drop(columns = ['Group_M_Crit200'])
-np.testing.assert_allclose(10**dmo_merged_df.R200c, dmo_merged_df.Group_R_Crit200, rtol = 1e-3)
-dmo_merged_df = dmo_merged_df.drop(columns = ['Group_R_Crit200'])
-
-# ----------- Read in properties from tng particle data
-particle_df = pd.read_hdf(data_path + halo_particles_file)
-particle_df = particle_df.drop(columns = ['m200c'])
-
-dmo_merged_df = pd.merge(dmo_merged_df, particle_df, on=['ID_DMO'])
-# Add velocity anisotropy
-
-anisotropy_df = pd.read_hdf(anisotropy_path)
-anisotropy_df = anisotropy_df.drop(columns=['index'])
-
-dmo_merged_df = pd.merge(dmo_merged_df, anisotropy_df, on=['ID_DMO'])
-
-# Add r2500c values
-inner_df = pd.read_hdf(data_path + inner_file)
+# Check the merged haloes are the correct ones ----------------------------------
 np.testing.assert_allclose(
-    inner_df.m200c, 10**dmo_merged_df.Group_M_Crit200, rtol = 1e-3
+    dmo_merged_df.M200_DMO, dmo_merged_df.Group_M_Crit200, rtol=1e-3
 )
-inner_df = inner_df.drop(columns = ['m200c'])
-inner_df = inner_df["m2500c"].apply(np.log10)
+dmo_merged_df = dmo_merged_df.drop(columns = ['Group_M_Crit200','Group_R_Crit200'])
 
-dmo_merged_df = pd.merge(dmo_merged_df, inner_df, on=['ID_DMO'])
+# Read Particle Data ------------------------------------------------------------
+# R200c
+pouter_df = pd.read_hdf(particle_outer_file)
+np.testing.assert_allclose(
+    pouter_df.m200c, pouter_df.Group_M_Crit200, rtol=1e-3
+)
+pouter_df = particle_df.drop(columns = ['m200c'])
+dmo_df = pd.merge(dmo_df, pouter_df, on=['ID_DMO'])
 
-# ----------- Read in properties from tng hydro 
-hydro_df = pd.read_hdf(data_path + hydro_file)
+# R2500c
+pinner_df = pd.read_hdf(particle_inner_file)
+np.testing.assert_allclose(
+    inner_df.m200c, dmo_merged_df.Group_M_Crit200, rtol=1e-3
+)
+pinner_df = pinner_df.drop(columns = ['m200c'])
 
-hydro_merged_df = pd.merge(dmo_merged_df, hydro_df, on = ['ID_HYDRO'], how = 'inner', suffixes = ('_dmo', '_hydro'))
+dmo_df = pd.merge(dmo_df, pinner_df, on=['ID_DMO'])
 
-np.testing.assert_allclose(hydro_merged_df.M200_HYDRO, 10**hydro_merged_df.Group_M_Crit200, rtol = 1e-3)
+# Read in properties from tng hydro ---------------------------------------------
+hydro_df = pd.read_hdf(subfind_hydro_file)
+
+hydro_merged_df = pd.merge(
+    dmo_merged_df,
+    hydro_df,
+    on=['ID_HYDRO'],
+    how='inner',
+    suffixes=('_dmo', '_hydro')
+)
+
+np.testing.assert_allclose(
+    hydro_merged_df.M200_HYDRO, hydro_merged_df.Group_M_Crit200, rtol=1e-3
+)
 hydro_merged_df = hydro_merged_df.drop(columns = ['Group_M_Crit200', 'M200_DMO'])
 hydro_merged_df.M200c = np.log10(hydro_merged_df.M200c)
 
